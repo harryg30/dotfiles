242690be482ee7fa7275e3129944f8601ae15300
Harry Gordenstein <gordensteiharry@gordensteiharry-mbp.dhcp.thefacebook.com>
2024-03-04 18:30:17 -0500

    thefuck

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 echo "[INSTALL] Powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-echo "[SETUP] TheFuck"
-sudo apt update -y
-sudo apt install python3-dev python3-pip python3-setuptools -y
-pip3 install thefuck --user
+# echo "[SETUP] TheFuck"
+# sudo apt update -y
+# sudo apt install python3-dev python3-pip python3-setuptools -y
+# pip3 install thefuck --user
 
 echo "[SETUP] Vim"
 sudo apt-get install vim -y
